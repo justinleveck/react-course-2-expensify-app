033bbfe85c2d32bf537c348bf4797bc720a27bf7
Justin Leveck <jleveck@gmail.com>
2018-05-15 08:09:16 -0700

    Remove console log

--- src/app.js
+++ src/app.js
@@ -11,10 +11,8 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
