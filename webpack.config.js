567a3ac0ae08d860d2510d91c1ab0bd27e71b656
Justin Leveck <jleveck@gmail.com>
2018-05-15 10:43:24 -0700

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,12 @@ module.exports = env => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
-      rules: [{
+      rules: [
+        {
           loader: 'babel-loader',
           test: /\.*js$/,
           exclude: /node_modules/
@@ -20,7 +21,8 @@ module.exports = env => {
         {
           test: /\.s?css$/,
           use: CSSExtract.extract({
-            use: [{
+            use: [
+              {
                 loader: 'css-loader',
                 options: {
                   sourceMap: true
@@ -41,7 +43,8 @@ module.exports = env => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 };
